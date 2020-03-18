71e85e759f1e1891017b9caa6768a39295dd8b8b
moris <moris.rafol@gmail.com>
2020-03-18 15:21:02 +0200

    f:model.js - added playerObj object f:model.js - added gamesRules object f:model.js - implemented the timer function

--- scripts/models.js
+++ scripts/models.js
@@ -17,13 +17,65 @@ class questionData{
     }
 
 }
-
-
 let arr=[
     new questionData("what is 2 + 2 ?", 0,"4","5", "2"),
     new questionData("what is the capital Italy?",1 ,"haifa","rome", "madrid"),
 ];
 
 
-console.log(arr[0].question);
-console.log(arr[1].getAnswer());
+
+//-----------------------------------------------------------------------------------------
+var currentCountdown;
+var intervalID;
+/***
+ * this function is used to show a countdown and update the DOM
+ * @param seconds - the countdown in seconds
+ * @param func - the function that will be called every second of the countdown
+ */
+function timer(seconds, func){
+    currentCountdown = seconds;
+    intervalID= setInterval(()=>{
+
+        func();    //update the DOM
+        currentCountdown--;
+
+        if(currentCountdown < 0)
+            clearInterval(intervalID);
+
+    },1000)
+}
+function calculateScoreForRound(){
+    return currentCountdown *gameRulesObj.getScore();
+}
+//-----------------------------------------------------------------------------------------
+
+let playerObj ={
+    playerName:"",
+    playerScore: 0,
+
+}
+let gameRulesObj={
+    scorePerRound_easyMode: 10,
+    scorePerRound_mediumMode: 20,
+    scorePerRound_hardMode: 30,
+    roundType:"easy",
+    getScore(){
+        switch (this.roundType.toLowerCase()) {
+            case "easy":
+                return this.scorePerRound_easyMode;
+            case "medium":
+                return this.scorePerRound_mediumMode;
+            case "hard":
+                return this.scorePerRound_hardMode;
+
+        }
+    },
+    setModeType(mode){
+        this.roundType = mode;
+    }
+
+
+}
+
+
+gameRulesObj.setModeType("hard");
